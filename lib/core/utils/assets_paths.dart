277fcc23f590b65561160724c53860313bf3a8f2
Muhammad Qaasim <q.akorede@featuremind.com>
2025-05-14 20:33:06 +0100

    feat: implement search functionality with contact list UI

--- lib/core/utils/assets_paths.dart
+++ lib/core/utils/assets_paths.dart
@@ -7,6 +7,7 @@ class AssetsPaths {
   static const String icSearch = '$_svgsDir/ic_search.svg';
   static const String icAdd = '$_svgsDir/ic_add.svg';
   static const String icDelivered = '$_svgsDir/ic_delivered.svg';
+  static const String icVerifiedUser = '$_svgsDir/ic_verified_user.svg';
 
   //PNGS
   static const String icImage = '$_pngsDir/ic_image.png';
