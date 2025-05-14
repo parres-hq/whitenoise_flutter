de2b6604704f0bc273db3227e7c0575072e4c802
Muhammad Qaasim <q.akorede@featuremind.com>
2025-05-14 09:12:05 +0100

    feat: added some assets and util classes

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+  static const Color color202320 = Color(0xFF202320);
+  static const Color color727772 = Color(0xFF727772);
+}
---
+++ lib/core/utils/assets_paths.dart
@@ -0,0 +1,10 @@
+class AssetsPaths {
+  static const String _svgsDir = 'assets/svgs';
+  static const String _pngsDir = 'assets/pngs';
+
+  static const String icChat = '$_svgsDir/ic_chat.svg';
+  static const String icSearch = '$_svgsDir/ic_search.svg';
+  static const String icAdd = '$_svgsDir/ic_add.svg';
+
+  static const String icImage = '$_pngsDir/ic_image.png';
+}
