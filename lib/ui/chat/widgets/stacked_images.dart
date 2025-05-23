72f716f2aca2970cd88485726e59eea54bbbc0c4
nextidearly <talenteddev0125@gmail.com>
2025-05-23 07:59:13 -0700

    Fix image picking

--- lib/ui/chat/widgets/stacked_images.dart
+++ lib/ui/chat/widgets/stacked_images.dart
@@ -17,7 +17,7 @@ class StackedImages extends StatelessWidget {
     super.key,
     required this.imageUris,
     required this.onDelete,
-    this.imageSize = 200,
+    this.imageSize = 60,
     this.overlapPercentage = 0.15, // 15% overlap
     this.maxImagesToShow = 5,
   });
@@ -32,68 +32,75 @@ class StackedImages extends StatelessWidget {
     final int imagesToShow = min(imageUris.length, maxImagesToShow);
     final double totalWidth = responsiveImageSize + (overlap * (imagesToShow - 1));
 
-    return SizedBox(
-      height: responsiveImageSize,
-      width: totalWidth,
-      child: Stack(
-        clipBehavior: Clip.none,
+    return Padding(
+      padding: const EdgeInsets.only(left: 8),
+      child: Row(
         children: [
-          // Display the stacked images
-          ...List.generate(imagesToShow, (index) {
-            final uri = imageUris[index];
-            final double rotation = Random().nextDouble() * 10 - 5;
-            final double offsetX = index * overlap;
+          SizedBox(
+            height: responsiveImageSize,
+            width: totalWidth,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                // Display the stacked images
+                ...List.generate(imagesToShow, (index) {
+                  final uri = imageUris[index];
+                  final double rotation = Random().nextDouble() * 10 - 5;
+                  final double offsetX = index * overlap;
 
-            return Positioned(
-              left: offsetX,
-              child: Transform.rotate(
-                angle: rotation * (pi / 180),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.r),
-                  child: _buildImage(uri, responsiveImageSize),
-                ),
-              ),
-            );
-          }),
+                  return Positioned(
+                    left: offsetX,
+                    child: Transform.rotate(angle: rotation * (pi / 180), child: _buildImage(uri, responsiveImageSize)),
+                  );
+                }),
 
-          // Delete button positioned on top of the last image
-          if (imageUris.isNotEmpty)
-            Positioned(
-              top: 8.h,
-              right: 8.w,
-              child: GestureDetector(
-                onTap: onDelete,
-                child: Container(
-                  width: 24.w,
-                  height: 24.h,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.white.withOpacity(0.9),
-                    boxShadow: [
-                      BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 4.r, offset: Offset(0, 2.h)),
-                    ],
+                // Delete button positioned on top of the last image
+                if (imageUris.isNotEmpty)
+                  Positioned(
+                    top: 6.h,
+                    right: 2.w,
+                    child: GestureDetector(
+                      onTap: onDelete,
+                      child: Container(
+                        width: 20.w,
+                        height: 20.h,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.white.withOpacity(0.9),
+                          boxShadow: [BoxShadow(color: AppColors.glitch600, blurRadius: 4.r, offset: Offset(0, 2.h))],
+                        ),
+                        child: Icon(Icons.close, size: 12.sp, color: Colors.red),
+                      ),
+                    ),
                   ),
-                  child: Icon(Icons.close, size: 16.sp, color: Colors.red),
-                ),
-              ),
+              ],
             ),
+          ),
         ],
       ),
     );
   }
 
   Widget _buildImage(String uri, double size) {
-    return uri.startsWith("http")
-        ? CachedNetworkImage(
-          imageUrl: uri,
-          fit: BoxFit.cover,
-          width: size,
-          height: size,
-          placeholder: (context, url) => Container(color: Colors.grey[200], width: size, height: size),
-          errorWidget:
-              (context, url, error) =>
-                  Container(color: Colors.grey[300], width: size, height: size, child: Icon(Icons.error, size: 24.sp)),
-        )
-        : Image.file(File(uri), fit: BoxFit.cover, width: size, height: size);
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: AppColors.glitch80, width: 4)),
+      child:
+          uri.startsWith("http")
+              ? CachedNetworkImage(
+                imageUrl: uri,
+                fit: BoxFit.cover,
+                width: size,
+                height: size,
+                placeholder: (context, url) => Container(color: Colors.grey[200], width: size, height: size),
+                errorWidget:
+                    (context, url, error) => Container(
+                      color: Colors.grey[300],
+                      width: size,
+                      height: size,
+                      child: Icon(Icons.error, size: 24.sp),
+                    ),
+              )
+              : Image.file(File(uri), fit: BoxFit.cover, width: size, height: size),
+    );
   }
 }
