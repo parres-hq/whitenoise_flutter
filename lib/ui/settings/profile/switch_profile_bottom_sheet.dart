4f59b8658f751895f5375642492d460d000e9bb8
Muhammad Qaasim <qaasim405@gmail.com>
2025-07-02 17:25:30 +0100

    feat: connect profile bottomSheet withe create new account impl

--- lib/ui/settings/profile/switch_profile_bottom_sheet.dart
+++ lib/ui/settings/profile/switch_profile_bottom_sheet.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:whitenoise/config/extensions/toast_extension.dart';
 import 'package:whitenoise/domain/models/contact_model.dart';
 import 'package:whitenoise/ui/contact_list/widgets/contact_list_tile.dart';
+import 'package:whitenoise/ui/core/ui/app_button.dart';
 import 'package:whitenoise/ui/core/ui/custom_bottom_sheet.dart';
+import 'package:whitenoise/ui/settings/profile/connect_profile_bottom_sheet.dart';
 
-class SwitchProfileBottomSheet extends StatelessWidget {
+class SwitchProfileBottomSheet extends ConsumerWidget {
   final List<ContactModel> profiles;
   final Function(ContactModel) onProfileSelected;
 
@@ -21,9 +27,9 @@ class SwitchProfileBottomSheet extends StatelessWidget {
   }) {
     return CustomBottomSheet.show(
       context: context,
-      title: 'Switch profile',
+      title: 'Profiles',
       wrapContent: true,
-      maxHeight: 0.6.sh,
+      maxHeight: 0.65.sh,
       builder:
           (context) => SwitchProfileBottomSheet(
             profiles: profiles,
@@ -33,21 +39,43 @@ class SwitchProfileBottomSheet extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      padding: EdgeInsets.symmetric(horizontal: 24.w),
-      itemCount: profiles.length,
-      itemBuilder: (context, index) {
-        final profile = profiles[index];
-        return ContactListTile(
-          contact: profile,
-          onTap: () {
-            onProfileSelected(profile);
-            Navigator.pop(context);
-          },
-        );
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Flexible(
+          child: ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            itemCount: profiles.length,
+            itemBuilder: (context, index) {
+              final profile = profiles[index];
+              return ContactListTile(
+                contact: profile,
+                onTap: () {
+                  if (profiles.length > 1) {
+                    onProfileSelected(profile);
+                    Navigator.pop(context);
+                  } else {
+                    ref.showRawErrorToast('You must have at least one profile to switch to.');
+                  }
+                },
+              );
+            },
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: AppFilledButton(
+            title: 'Connect Another Profile',
+            onPressed: () {
+              context.pop();
+              ConnectProfileBottomSheet.show(context: context);
+            },
+          ),
+        ),
+        Gap(8.h),
+      ],
     );
   }
 }
