773f495b80fa55823c44bc806943c3c326485eba
codeswot <elmubarak333@gmail.com>
2025-08-06 15:18:57 +0100

    include add/remove relay methods for relay types

--- lib/ui/auth_flow/info_screen.dart
+++ lib/ui/auth_flow/info_screen.dart
@@ -41,6 +41,7 @@ class _InfoScreenState extends ConsumerState<InfoScreen> {
     while (true) {
       await accountNotifier.loadAccountData();
       final accountState = ref.read(accountProvider);
+
       if (accountState.metadata?.displayName != null &&
           accountState.metadata!.displayName!.isNotEmpty) {
         break;
@@ -113,13 +114,12 @@ class _InfoScreenState extends ConsumerState<InfoScreen> {
           ).copyWith(bottom: 32.h),
           child: Consumer(
             builder: (context, ref, child) {
-              // final accountState = ref.watch(accountProvider);
-              // final isButtonDisabled = _isLoading || accountState.isLoading;
+              final accountState = ref.watch(accountProvider);
+              final isButtonDisabled = _isLoading || accountState.isLoading;
 
               return WnFilledButton.child(
-                onPressed: () => _onContinuePressed(context),
-                // loading: isButtonDisabled,
-                // onPressed: isButtonDisabled ? null : () => _onContinuePressed(context),
+                loading: isButtonDisabled,
+                onPressed: isButtonDisabled ? null : () => _onContinuePressed(context),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
