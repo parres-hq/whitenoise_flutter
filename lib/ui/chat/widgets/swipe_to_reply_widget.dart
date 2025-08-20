366f2aaa53ba880864296c4f1629883f21187256
Mubarak Ibrahim <elmubarak333@gmail.com>
2025-08-20 14:36:58 +0100

    refactor swipe to reply widget (#520)

--- lib/ui/chat/widgets/swipe_to_reply_widget.dart
+++ lib/ui/chat/widgets/swipe_to_reply_widget.dart
@@ -28,44 +28,57 @@ class SwipeToReplyWidget extends StatefulWidget {
 
 class _SwipeToReplyWidgetState extends State<SwipeToReplyWidget> {
   double _dragExtent = 0.0;
-  final double _dragThreshold = 60.0;
+  static const double _dragThreshold = 60.0;
+  static const double _hapticThreshold = _dragThreshold * 0.5;
+  static const double _maxDragExtent = _dragThreshold * 1.2;
   bool _showReplyIcon = false;
   bool _hapticTriggered = false;
+  bool _canUndo = false;
   Timer? _longPressTimer;
 
   void _handleDragStart(DragStartDetails details) {
     _longPressTimer?.cancel();
     setState(() {
       _showReplyIcon = true;
+      _canUndo = false;
     });
   }
 
   void _handleDragUpdate(DragUpdateDetails details) {
-    if (details.delta.dx > 0) {
-      setState(() {
-        _dragExtent += details.delta.dx;
-        _dragExtent = _dragExtent.clamp(
-          0.0,
-          _dragThreshold * 1.2,
-        );
+    final double delta = details.delta.dx;
+    if (delta > 0 || (delta < 0 && _dragExtent > 0)) {
+      final double newDragExtent = (_dragExtent + delta).clamp(0.0, _maxDragExtent).toDouble();
 
-        if (_dragExtent >= _dragThreshold * 0.5 && !_hapticTriggered) {
+      final bool crossedThreshold = _dragExtent >= _hapticThreshold;
+      final bool belowThreshold = newDragExtent < _hapticThreshold;
+
+      setState(() {
+        _dragExtent = newDragExtent;
+
+        if (newDragExtent >= _hapticThreshold && !_hapticTriggered) {
           HapticFeedback.lightImpact();
           _hapticTriggered = true;
+        } else if (belowThreshold) {
+          if (crossedThreshold) _canUndo = true;
+          _hapticTriggered = false;
         }
       });
     }
   }
 
   void _handleDragEnd(DragEndDetails details) {
-    if (_dragExtent >= _dragThreshold * 0.5) {
+    if (_dragExtent >= _hapticThreshold && !_canUndo) {
       widget.onReply();
     }
+    _resetState();
+  }
 
+  void _resetState() {
     setState(() {
       _dragExtent = 0.0;
       _showReplyIcon = false;
       _hapticTriggered = false;
+      _canUndo = false;
     });
   }
 
@@ -91,51 +104,56 @@ class _SwipeToReplyWidgetState extends State<SwipeToReplyWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final dragOffset = _dragExtent;
     return Stack(
       children: [
-        if (_showReplyIcon)
-          Positioned(
-            left: 8.w,
-            top: 0,
-            bottom: widget.message.reactions.isNotEmpty ? 18.h : 0,
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: AnimatedScale(
-                scale: _dragExtent > _dragThreshold * 0.5 ? 1.2 : 1.0,
-                duration: const Duration(milliseconds: 100),
-                child: Container(
-                  padding: EdgeInsets.all(6.w),
-                  decoration: BoxDecoration(
-                    color: context.colors.primary.withValues(alpha: 0.1),
-                    shape: BoxShape.circle,
-                  ),
-                  child: SvgPicture.asset(
-                    AssetsPaths.icReply,
-                    colorFilter: ColorFilter.mode(
-                      context.colors.primary,
-                      BlendMode.srcIn,
-                    ),
-                    width: 16.w,
-                    height: 16.w,
-                  ),
-                ),
-              ),
+        if (_showReplyIcon) _buildReplyIcon(context),
+        SizedBox(
+          width: double.infinity,
+          child: GestureDetector(
+            onTapDown: _onTapDown,
+            onTapUp: _onTapUp,
+            onTapCancel: _onTapCancel,
+            onHorizontalDragStart: _handleDragStart,
+            onHorizontalDragUpdate: _handleDragUpdate,
+            onHorizontalDragEnd: _handleDragEnd,
+            child: Transform.translate(
+              offset: Offset(_dragExtent, 0),
+              child: widget.child,
             ),
-          ),
-        GestureDetector(
-          onTapDown: _onTapDown,
-          onTapUp: _onTapUp,
-          onTapCancel: _onTapCancel,
-          onHorizontalDragStart: _handleDragStart,
-          onHorizontalDragUpdate: _handleDragUpdate,
-          onHorizontalDragEnd: _handleDragEnd,
-          child: Transform.translate(
-            offset: Offset(dragOffset, 0),
-            child: widget.child,
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildReplyIcon(BuildContext context) {
+    return Positioned(
+      left: 8.w,
+      top: 0,
+      bottom: widget.message.reactions.isNotEmpty ? 18.h : 0,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: AnimatedScale(
+          scale: _dragExtent > _hapticThreshold ? 1.2 : 1.0,
+          duration: const Duration(milliseconds: 100),
+          child: Container(
+            padding: EdgeInsets.all(6.w),
+            decoration: BoxDecoration(
+              color: context.colors.primary.withValues(alpha: 0.1),
+              shape: BoxShape.circle,
+            ),
+            child: SvgPicture.asset(
+              AssetsPaths.icReply,
+              colorFilter: ColorFilter.mode(
+                context.colors.primary,
+                BlendMode.srcIn,
+              ),
+              width: 16.w,
+              height: 16.w,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
