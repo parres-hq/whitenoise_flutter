5f1398532196b8f84c8283b741a8c078c56ff2da
josefinalliende <moalliende@uc.cl>
2025-09-04 13:27:49 -0400

    chore: remove unused code

--- lib/domain/models/contact_model.dart
+++ lib/domain/models/contact_model.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:whitenoise/src/rust/api/metadata.dart' show FlutterMetadata;
-import 'package:whitenoise/src/rust/api/users.dart' show User;
 import 'package:whitenoise/utils/public_key_validation_extension.dart';
 
 class ContactModel {
@@ -39,28 +38,6 @@ class ContactModel {
     return ContactModel(
       displayName: displayName.isNotEmpty ? displayName : 'Unknown User',
       publicKey: npub,
-      imagePath: picture,
-      about: about,
-      website: website,
-      nip05: nip05,
-      lud16: lud16,
-    );
-  }
-
-  factory ContactModel.fromUser({
-    required User user,
-  }) {
-    final metadata = user.metadata;
-    final displayName = _sanitizeString(metadata.displayName);
-    final about = _sanitizeString(metadata.about);
-    final website = _sanitizeUrl(metadata.website);
-    final nip05 = _sanitizeString(metadata.nip05);
-    final lud16 = _sanitizeString(metadata.lud16);
-    final picture = _sanitizeUrl(metadata.picture);
-
-    return ContactModel(
-      displayName: displayName.isNotEmpty ? displayName : 'Unknown User',
-      publicKey: user.pubkey,
       imagePath: picture,
       about: about,
       website: website,
