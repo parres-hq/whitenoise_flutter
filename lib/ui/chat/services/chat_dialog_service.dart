e9e3a2f2a7d24cb4be6c022b08dc92b835598a31
Quwaysim <qaasim405@gmail.com>
2025-09-27 10:18:28 +0100

    ran precommit

--- lib/ui/chat/services/chat_dialog_service.dart
+++ lib/ui/chat/services/chat_dialog_service.dart
@@ -1,6 +1,5 @@
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whitenoise/config/providers/chat_provider.dart';
