c8df2b1112eb911eeab687e03ddc37def7faac62
josefinalliende <mariajosefinaalliende@gmail.com>
2025-10-10 13:44:19 -0300

    fixup! refactor: extract tag building logic to its own tested class

--- lib/domain/services/nostr_tag_builder_service.dart
+++ lib/domain/services/nostr_tag_builder_service.dart
@@ -8,6 +8,11 @@ class NostrTagBuilderService {
     Future<Tag> Function({required List<String> vec})? tagFromVecFn,
   }) : _tagFromVecFn = tagFromVecFn ?? utils.tagFromVec;
 
+  // NIP-25 compliant reaction tags
+  // According to NIP-25:
+  // - MUST have e tag with event id being reacted to
+  // - SHOULD have p tag with pubkey of event being reacted to
+  // - SHOULD have k tag with stringified kind number of reacted event
   Future<List<Tag>> buildReactionTags({
     required String messageId,
     required String messagePubkey,
@@ -35,8 +40,8 @@ class NostrTagBuilderService {
   }) async {
     return [
       await _tagFromVecFn(vec: ['e', messageId]),
-      await _tagFromVecFn(vec: ['p', messagePubkey]),
-      await _tagFromVecFn(vec: ['k', messageKind.toString()]),
+      await _tagFromVecFn(vec: ['p', messagePubkey]), // Author of the message being deleted
+      await _tagFromVecFn(vec: ['k', messageKind.toString()]), // Kind of the message being deleted
     ];
   }
 }
