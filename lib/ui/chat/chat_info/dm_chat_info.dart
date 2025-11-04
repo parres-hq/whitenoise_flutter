45d4b5821e1dcda011fa97435eec681898864bda
codeswot <elmubarak333@gmail.com>
2025-11-04 15:03:25 +0100

    Refactor DMChatInfo layout for improved readability and maintainability

--- lib/ui/chat/chat_info/dm_chat_info.dart
+++ lib/ui/chat/chat_info/dm_chat_info.dart
@@ -60,105 +60,107 @@ class DMChatInfo extends ConsumerWidget {
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 32.w),
-      child: Column(
-        children: [
-          Gap(64.h),
-          WnAvatar(
-            imageUrl: otherMember?.imagePath ?? '',
-            displayName: otherMember?.displayName ?? 'chats.unknown'.tr(),
-            size: 96.w,
-          ),
-          SizedBox(height: 16.h),
-          Text(
-            otherMember?.displayName ?? 'chats.unknown'.tr(),
-            style: context.textTheme.bodyLarge?.copyWith(
-              color: context.colors.primary,
-              fontSize: 18.sp,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Gap(64.h),
+            WnAvatar(
+              imageUrl: otherMember?.imagePath ?? '',
+              displayName: otherMember?.displayName ?? 'chats.unknown'.tr(),
+              size: 96.w,
             ),
-          ),
-          Gap(2.h),
-          Text(
-            otherMember?.nip05 ?? '',
-            style: context.textTheme.bodyMedium?.copyWith(
-              color: context.colors.mutedForeground,
-              fontSize: 14.sp,
+            SizedBox(height: 16.h),
+            Text(
+              otherMember?.displayName ?? 'chats.unknown'.tr(),
+              style: context.textTheme.bodyLarge?.copyWith(
+                color: context.colors.primary,
+                fontSize: 18.sp,
+              ),
             ),
-          ),
-          Gap(16.h),
-          Row(
-            children: [
-              Flexible(
-                child: Text(
-                  otherMember?.publicKey.formatPublicKey() ?? '',
-                  textAlign: TextAlign.center,
-                  style: context.textTheme.bodyMedium?.copyWith(
-                    color: context.colors.mutedForeground,
-                    fontSize: 14.sp,
+            Gap(2.h),
+            Text(
+              otherMember?.nip05 ?? '',
+              style: context.textTheme.bodyMedium?.copyWith(
+                color: context.colors.mutedForeground,
+                fontSize: 14.sp,
+              ),
+            ),
+            Gap(16.h),
+            Row(
+              children: [
+                Flexible(
+                  child: Text(
+                    otherMember?.publicKey.formatPublicKey() ?? '',
+                    textAlign: TextAlign.center,
+                    style: context.textTheme.bodyMedium?.copyWith(
+                      color: context.colors.mutedForeground,
+                      fontSize: 14.sp,
+                    ),
                   ),
                 ),
-              ),
-              Gap(8.w),
-              InkWell(
-                onTap:
-                    otherUserPubkey != null ? () => _copyToClipboard(ref, otherUserPubkey) : null,
-                child: WnImage(
-                  AssetsPaths.icCopy,
-                  width: 24.w,
-                  height: 24.w,
-                  color: context.colors.primary,
+                Gap(8.w),
+                InkWell(
+                  onTap:
+                      otherUserPubkey != null ? () => _copyToClipboard(ref, otherUserPubkey) : null,
+                  child: WnImage(
+                    AssetsPaths.icCopy,
+                    width: 24.w,
+                    height: 24.w,
+                    color: context.colors.primary,
+                  ),
                 ),
+              ],
+            ),
+            Gap(32.h),
+            WnFilledButton(
+              size: WnButtonSize.small,
+              visualState: WnButtonVisualState.secondary,
+              label: 'ui.searchChat'.tr(),
+              suffixIcon: WnImage(
+                AssetsPaths.icSearch,
+                width: 14.w,
+                color: context.colors.secondaryForeground,
               ),
-            ],
-          ),
-          Gap(32.h),
-          WnFilledButton(
-            size: WnButtonSize.small,
-            visualState: WnButtonVisualState.secondary,
-            label: 'ui.searchChat'.tr(),
-            suffixIcon: WnImage(
-              AssetsPaths.icSearch,
-              width: 14.w,
-              color: context.colors.secondaryForeground,
+              onPressed: () {
+                ref.read(chatSearchProvider(groupId).notifier).activateSearch();
+                context.pop();
+              },
             ),
-            onPressed: () {
-              ref.read(chatSearchProvider(groupId).notifier).activateSearch();
-              context.pop();
-            },
-          ),
-          Gap(8.h),
-          WnFilledButton(
-            size: WnButtonSize.small,
-            visualState:
-                followState.isFollowing
-                    ? WnButtonVisualState.secondary
-                    : WnButtonVisualState.primary,
-            label: followState.isFollowing ? 'ui.unfollow'.tr() : 'ui.follow'.tr(),
-            loading: followState.isLoading,
-            suffixIcon: WnImage(
-              followState.isFollowing ? AssetsPaths.icRemoveUser : AssetsPaths.icAddUser,
-              width: 14.w,
-              color:
+            Gap(8.h),
+            WnFilledButton(
+              size: WnButtonSize.small,
+              visualState:
                   followState.isFollowing
-                      ? context.colors.secondaryForeground
-                      : context.colors.primaryForeground,
+                      ? WnButtonVisualState.secondary
+                      : WnButtonVisualState.primary,
+              label: followState.isFollowing ? 'ui.unfollow'.tr() : 'ui.follow'.tr(),
+              loading: followState.isLoading,
+              suffixIcon: WnImage(
+                followState.isFollowing ? AssetsPaths.icRemoveUser : AssetsPaths.icAddUser,
+                width: 14.w,
+                color:
+                    followState.isFollowing
+                        ? context.colors.secondaryForeground
+                        : context.colors.primaryForeground,
+              ),
+              onPressed:
+                  followState.isLoading ? null : () => _toggleFollow(ref, otherUserPubkey, context),
             ),
-            onPressed:
-                followState.isLoading ? null : () => _toggleFollow(ref, otherUserPubkey, context),
-          ),
-          Gap(8.h),
-          WnFilledButton(
-            size: WnButtonSize.small,
-            visualState: WnButtonVisualState.secondary,
-            label: 'ui.addToGroup'.tr(),
-            suffixIcon: WnImage(
-              AssetsPaths.icAdd,
-              width: 14.w,
-              color: context.colors.secondaryForeground,
+            Gap(8.h),
+            WnFilledButton(
+              size: WnButtonSize.small,
+              visualState: WnButtonVisualState.secondary,
+              label: 'ui.addToGroup'.tr(),
+              suffixIcon: WnImage(
+                AssetsPaths.icAdd,
+                width: 14.w,
+                color: context.colors.secondaryForeground,
+              ),
+              onPressed:
+                  otherUserPubkey != null ? () => _openAddToGroup(context, otherUserPubkey) : null,
             ),
-            onPressed:
-                otherUserPubkey != null ? () => _openAddToGroup(context, otherUserPubkey) : null,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
