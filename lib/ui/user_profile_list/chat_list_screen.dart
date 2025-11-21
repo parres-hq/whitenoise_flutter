5445c5bc01e75bdf5efa616dd8d812efb48d6901
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-11-21 07:45:46 -0600

    Disable in-app polling for debugging

--- lib/ui/user_profile_list/chat_list_screen.dart
+++ lib/ui/user_profile_list/chat_list_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -111,7 +112,11 @@ class _ChatListScreenState extends ConsumerState<ChatListScreen>
       WelcomeNotificationService.initialize(context);
       WelcomeNotificationService.setupWelcomeNotifications(ref);
       _loadData();
-      _pollingNotifier.startPolling();
+      if (!Platform.isIOS) {
+        _pollingNotifier.startPolling();
+      } else {
+        _log.info('Skipping pollingNotifier.startPolling on iOS (debugging backgrounding issue).');
+      }
     });
   }
 
